8236465df552956c5b6c44ca7a2d58745051a1ba
matiss <matissjurevics@gmail.com>
2022-06-14 20:18:52 +0100

    added the initBoard function

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const canvas = document.getElementById("canvas")
+canvas.style.background = "#E3E4DB"
+const context = canvas.getContext('2d');
+
+context.clearRect(0, 0, canvas.width, canvas.height)
+
+const initBoard = () => {
+    // itirate through rows on the board
+    for (let i = 0; i < 8; i++) {
+        // itirate through columns on the board
+        for (let j = 0; j < 8 ; j++) {
+            context.fillStyle = "#493843"
+            
+            if(i%2==1) {
+                if(j%2==1) {
+                    context.fillRect(j*100, i*100, 100 , 100)
+                }
+            } else {
+                if(j%2==0) {
+                    context.fillRect(j*100, i*100, 100 , 100)
+                }
+            }
+        }
+    }
+}
+
+initBoard()
