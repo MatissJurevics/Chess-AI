e528394b8b57d1f8ac1b3c50432ff78064b87bdb
matiss <matissjurevics@gmail.com>
2022-06-14 20:36:21 +0100

    rendered chess piece positions (without images)

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const initBoard = () => {
         // itirate through columns on the board
         for (let j = 0; j < 8 ; j++) {
             context.fillStyle = "#493843"
-            
+            // Applies one condition on every other row
             if(i%2==1) {
                 if(j%2==1) {
                     context.fillRect(j*100, i*100, 100 , 100)
@@ -24,4 +24,38 @@ const initBoard = () => {
     }
 }
 
+
+let boardMap = [
+    [2,4,3,6,5,3,4,2],
+    [1,1,1,1,1,1,1,1],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,1],
+    [2,4,3,5,6,3,4,2],
+]
+let valueMap = {
+    1: "pawn",
+    2: "rook",
+    3: "bishop",
+    4: "knight",
+    5: "queen",
+    6: "king",
+}
+
+const loadPositions = () => {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if (boardMap[i][j]) {
+                context.fillStyle = "blue"
+                context.font = "15px arial"
+
+                context.fillText(valueMap[boardMap[i][j]], j*100 + 35, i*100 + 57.5)
+            }
+        }
+    }
+}
 initBoard()
+
+loadPositions()
