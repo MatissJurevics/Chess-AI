1bb152eabdeb54469a65be3435dda7164671f1d8
matiss <matissjurevics@gmail.com>
2022-06-14 20:54:43 +0100

    god I dont know anymore

---
+++ README.md
@@ -0,0 +1,8 @@
+# Chess AI
+A basic AI that can play chess
+
+## TO-DO
+[ ] Create chess board with moveable pieces
+[ ] Implement proper logic for all pieces (castling, etc)
+[ ] Create basic AI that moves at random
+[ ] Figure the rest out...
--- js/main.js
+++ js/main.js
@@ -1,61 +1,109 @@
-const canvas = document.getElementById("canvas")
-canvas.style.background = "#E3E4DB"
-const context = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+canvas.style.background = "#E3E4DB";
+const context = canvas.getContext("2d");
 
-context.clearRect(0, 0, canvas.width, canvas.height)
+context.clearRect(0, 0, canvas.width, canvas.height);
 
 const initBoard = () => {
-    // itirate through rows on the board
-    for (let i = 0; i < 8; i++) {
-        // itirate through columns on the board
-        for (let j = 0; j < 8 ; j++) {
-            context.fillStyle = "#493843"
-            // Applies one condition on every other row
-            if(i%2==1) {
-                if(j%2==1) {
-                    context.fillRect(j*100, i*100, 100 , 100)
-                }
-            } else {
-                if(j%2==0) {
-                    context.fillRect(j*100, i*100, 100 , 100)
-                }
-            }
+  // itirate through rows on the board
+  for (let i = 0; i < 8; i++) {
+    // itirate through columns on the board
+    for (let j = 0; j < 8; j++) {
+      context.fillStyle = "#493843";
+      // Applies one condition on every other row
+      if (i % 2 == 1) {
+        if (j % 2 == 1) {
+          context.fillRect(j * 100, i * 100, 100, 100);
         }
+      } else {
+        if (j % 2 == 0) {
+          context.fillRect(j * 100, i * 100, 100, 100);
+        }
+      }
     }
-}
-
+  }
+};
 
 let boardMap = [
-    [2,4,3,6,5,3,4,2],
-    [1,1,1,1,1,1,1,1],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [1,1,1,1,1,1,1,1],
-    [2,4,3,5,6,3,4,2],
-]
+  [2, 4, 3, 6, 5, 3, 4, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [7, 7, 7, 7, 7, 7, 7, 7],
+  [2, 4, 3, 5, 6, 3, 4, 2],
+];
 let valueMap = {
-    1: "pawn",
-    2: "rook",
-    3: "bishop",
-    4: "knight",
-    5: "queen",
-    6: "king",
-}
-
+  1: "bpawn",
+  2: "brook",
+  3: "bbishop",
+  4: "bknight",
+  5: "bqueen",
+  6: "bking",
+  7: "wpawn",
+};
+let BPAWN = new Image();
+BPAWN.src =
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.shareicon.net%2Fdata%2F2015%2F09%2F27%2F108210_game_512x512.png";
+let WPAWN = new Image();
+WPAWN.src =
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.vexels.com%2Fmedia%2Fusers%2F3%2F143290%2Fisolated%2Fpreview%2F3dc0e132939ad9ebff880b64637ea820-pawn-chess-figure-white-by-vexels.png&f=1&nofb=1";
 const loadPositions = () => {
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-            if (boardMap[i][j]) {
-                context.fillStyle = "blue"
-                context.font = "15px arial"
-
-                context.fillText(valueMap[boardMap[i][j]], j*100 + 35, i*100 + 57.5)
-            }
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      if (boardMap[i][j]) {
+        switch (valueMap[boardMap[i][j]]) {
+          case "bpawn":
+            context.drawImage(BPAWN, j * 100 + 25, i * 100 + 25, 50, 50);
+          case "wpawn":
+            context.drawImage(WPAWN, j * 100 + 25, i * 100 + 25, 50, 50);
+          case "rook":
+            context.fillStyle = "blue";
+            context.font = "15px arial";
+            context.fillText(
+              valueMap[boardMap[i][j]],
+              j * 100 + 35,
+              i * 100 + 57.5
+            );
+          case "bishop":
+            context.fillStyle = "blue";
+            context.font = "15px arial";
+            context.fillText(
+              valueMap[boardMap[i][j]],
+              j * 100 + 35,
+              i * 100 + 57.5
+            );
+          case "knight":
+            context.fillStyle = "blue";
+            context.font = "15px arial";
+            context.fillText(
+              valueMap[boardMap[i][j]],
+              j * 100 + 35,
+              i * 100 + 57.5
+            );
+          case "queen":
+            context.fillStyle = "blue";
+            context.font = "15px arial";
+            context.fillText(
+              valueMap[boardMap[i][j]],
+              j * 100 + 35,
+              i * 100 + 57.5
+            );
+          case "king":
+            context.fillStyle = "blue";
+            context.font = "15px arial";
+            context.fillText(
+              valueMap[boardMap[i][j]],
+              j * 100 + 35,
+              i * 100 + 57.5
+            );
         }
+      }
     }
-}
-initBoard()
+  }
+};
 
-loadPositions()
+initBoard();
+
+loadPositions();
