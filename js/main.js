74bff42698263d4aaa3cb5ba5cce0a08c85c61f5
matiss <matissjurevics@gmail.com>
2022-06-15 19:48:32 +0100

    adding FEN compatability

--- js/main.js
+++ js/main.js
@@ -24,86 +24,103 @@ const initBoard = () => {
   }
 };
 
+
+
+// For loadPieces()
+let setupString = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
 let boardMap = [
-  [2, 4, 3, 6, 5, 3, 4, 2],
-  [1, 1, 1, 1, 1, 1, 1, 1],
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
-  [7, 7, 7, 7, 7, 7, 7, 7],
-  [2, 4, 3, 5, 6, 3, 4, 2],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
 ];
 let valueMap = {
-  1: "bpawn",
-  2: "brook",
-  3: "bbishop",
-  4: "bknight",
-  5: "bqueen",
-  6: "bking",
-  7: "wpawn",
+  "p": "bpawn",
+  "r": "brook",
+  "b": "bbishop",
+  "n": "bknight",
+  "q": "bqueen",
+  "k": "bking",
+  "P": "wpawn",
+  "R": "wrook",
+  "B": "wbishop",
+  "N": "wknight",
+  "Q": "wqueen",
+  "K": "wking",
 };
-let BPAWN = new Image();
-BPAWN.src =
-  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.shareicon.net%2Fdata%2F2015%2F09%2F27%2F108210_game_512x512.png";
-let WPAWN = new Image();
-WPAWN.src =
-  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.vexels.com%2Fmedia%2Fusers%2F3%2F143290%2Fisolated%2Fpreview%2F3dc0e132939ad9ebff880b64637ea820-pawn-chess-figure-white-by-vexels.png&f=1&nofb=1";
-const loadPositions = () => {
-  for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-      if (boardMap[i][j]) {
-        switch (valueMap[boardMap[i][j]]) {
-          case "bpawn":
-            context.drawImage(BPAWN, j * 100 + 25, i * 100 + 25, 50, 50);
-          case "wpawn":
-            context.drawImage(WPAWN, j * 100 + 25, i * 100 + 25, 50, 50);
-          case "rook":
-            context.fillStyle = "blue";
-            context.font = "15px arial";
-            context.fillText(
-              valueMap[boardMap[i][j]],
-              j * 100 + 35,
-              i * 100 + 57.5
-            );
-          case "bishop":
-            context.fillStyle = "blue";
-            context.font = "15px arial";
-            context.fillText(
-              valueMap[boardMap[i][j]],
-              j * 100 + 35,
-              i * 100 + 57.5
-            );
-          case "knight":
-            context.fillStyle = "blue";
-            context.font = "15px arial";
-            context.fillText(
-              valueMap[boardMap[i][j]],
-              j * 100 + 35,
-              i * 100 + 57.5
-            );
-          case "queen":
-            context.fillStyle = "blue";
-            context.font = "15px arial";
-            context.fillText(
-              valueMap[boardMap[i][j]],
-              j * 100 + 35,
-              i * 100 + 57.5
-            );
-          case "king":
-            context.fillStyle = "blue";
-            context.font = "15px arial";
-            context.fillText(
-              valueMap[boardMap[i][j]],
-              j * 100 + 35,
-              i * 100 + 57.5
-            );
-        }
-      }
-    }
+
+const loadPositions = (j,i,char) => {
+  console.log("loading positions " + valueMap[char] + j + i )
+  switch (valueMap[char]) {
+    case "wpawn":
+      // context.drawImage(WPAWN, j * 100 + 25, i * 100 + 25, 50, 50);
+      context.fillText("Wpawn", j * 100 + 25, i * 100 + 25, 50, 50);
+
+      break;
+    case "bpawn":
+      context.fillText("Bpawn", j * 100 + 25, i * 100 + 25, 50, 50);
+      // context.drawImage(BPAWN, j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "brook":
+      context.fillStyle = "red";
+      context.fillText("Brook", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "wrook":
+      context.fillStyle = "red";
+      context.fillText("Wrook", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "bknight":
+      context.fillStyle = "red";
+      context.fillText("Bknight", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "wknight":
+      context.fillStyle = "red";
+      context.fillText("Wknight", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "wbishop":
+      context.fillStyle = "red";
+      context.fillText("Wbishop", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "bbishop":
+      context.fillStyle = "red";
+      context.fillText("Bbishop", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "bqueen":
+      context.fillStyle = "red";
+      context.fillText("Bqueen", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "wqueen":
+      context.fillStyle = "red";
+      context.fillText("Wqueen", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "bking":
+      context.fillStyle = "red";
+      context.fillText("Bking", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "wking":
+      context.fillStyle = "red";
+      context.fillText("Wking", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
   }
 };
+const setupFromFEN = (FEN) => {
+  let col = 0;
+  let row = 0;
+  FEN.split("").forEach((char, index) => {
+    switch (char) {
+      case "/":
+        row++;
+        break;
+      default:
+        loadPositions(col,row,char);
+    }
+    col++
+  })
+}
 
 initBoard();
-
-loadPositions();
+setupFromFEN(setupString);
