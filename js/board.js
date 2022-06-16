7491ae5ca09787aa702fa69b7ab77c4512c0b9eb
matiss <matissjurevics@gmail.com>
2022-06-16 13:58:13 +0100

    refactored and fixed

--- js/board.js
+++ js/board.js
@@ -1,4 +1,4 @@
-
+import { setupString, boardMap, valueMap } from "./data.js";
 
 const canvas = document.getElementById("canvas");
 canvas.style.background = "#E3E4DB";
@@ -13,14 +13,91 @@ export const initBoard = () => {
       context.fillStyle = "#493843";
       // Applies one condition on every other row
       if (i % 2 == 1) {
-        if (j % 2 == 1) {
+        if (j % 2 == 0) {
           context.fillRect(j * 100, i * 100, 100, 100);
         }
       } else {
-        if (j % 2 == 0) {
+        if (j % 2 == 1) {
           context.fillRect(j * 100, i * 100, 100, 100);
         }
       }
     }
   }
+};
+
+const loadPos = (j, i, char) => {
+  switch (valueMap[char]) {
+    case "wpawn":
+      // context.drawImage(WPAWN, j * 100 + 25, i * 100 + 25, 50, 50);
+      context.fillText("Wpawn", j * 100 + 25, i * 100 + 25, 50, 50);
+
+      break;
+    case "bpawn":
+      context.fillText("Bpawn", j * 100 + 25, i * 100 + 25, 50, 50);
+      // context.drawImage(BPAWN, j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "brook":
+      context.fillStyle = "red";
+      context.fillText("Brook", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "wrook":
+      context.fillStyle = "red";
+      context.fillText("Wrook", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "bknight":
+      context.fillStyle = "red";
+      context.fillText("Bknight", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "wknight":
+      context.fillStyle = "red";
+      context.fillText("Wknight", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "wbishop":
+      context.fillStyle = "red";
+      context.fillText("Wbishop", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "bbishop":
+      context.fillStyle = "red";
+      context.fillText("Bbishop", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "bqueen":
+      context.fillStyle = "red";
+      context.fillText("Bqueen", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "wqueen":
+      context.fillStyle = "red";
+      context.fillText("Wqueen", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "bking":
+      context.fillStyle = "red";
+      context.fillText("Bking", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+    case "wking":
+      context.fillStyle = "red";
+      context.fillText("Wking", j * 100 + 25, i * 100 + 25, 50, 50);
+      break;
+  }
+};
+
+let WPAWN = new Image()
+
+export const setupFromFEN = (FEN) => {
+  let col = 0;
+  let row = 0;
+  FEN.split("").forEach((char, index) => {
+    switch (char) {
+      case "/":
+        row++;
+        col = 0;
+        break;
+      default:
+        if (parseInt(char, 10).toString() === char) {
+          col += parseInt(char, 10);
+          break;
+        }
+        loadPos(col % 8, row, char);
+        col++;
+        break;
+    }
+  });
 };
--- js/main.js
+++ js/main.js
@@ -1,29 +1,8 @@
-import loadPos from "./init.js"
 import { boardMap, valueMap, setupString } from "./data.js"
-import  { initBoard, context }  from "./board.js"
+import  { initBoard, context, setupFromFEN }  from "./board.js"
 
 
-const setupFromFEN = (FEN) => {
-  let col = 0;
-  let row = 0;
-  FEN.split("").forEach((char, index) => {
-    switch (char) {
-      case "/":
-        row++;
-        col = 0
-        break;
-      default:
-        if (parseInt(char, 10).toString() === char) {
-          col += parseInt(char, 10);
-          break;
-        }
-        loadPos(col % 8, row, char);
-        col++;
-        break;
-    }
-    
-  });
-};
+
 
 initBoard();
 setupFromFEN(setupString);
