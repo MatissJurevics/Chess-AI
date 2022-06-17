ea64d5bc20fdb1d2f1216de5e9d6ccbd32a9ef75
matiss <matissjurevics@gmail.com>
2022-06-17 15:36:15 +0100

    made some safety precautions

--- js/piece.js
+++ js/piece.js
@@ -2,6 +2,7 @@ import { boardMap } from "./data.js"
 
 export const movePiece = (x,y,newx,newy) => {
   if (!boardMap[y][x]) {alert("no piece there"); return false}
+  if (newx > 7 || newy > 7 || newx < 0 || newy < 0) {alert("Out of bounds"); return false}
   boardMap[newy][newx] = boardMap[y][x]
   boardMap[y][x] = 0;
 }
