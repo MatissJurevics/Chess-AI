8f80c16f5b4e7a52904ab66ad00f8f4550913dea
matiss <matissjurevics@gmail.com>
2022-06-17 17:12:25 +0100

    everything is currently working perfectly

--- js/main.js
+++ js/main.js
@@ -14,17 +14,24 @@ setupFromFEN(setupString);
 
 let startx = 0;
 let starty = 0;
-let clicked = false;
+export let clicked = false;
 
 canvas.addEventListener("click", (e) => {
   if (!clicked) {
     startx = Math.floor(e.offsetX/100);
     starty = Math.floor(e.offsetY/100);
+    if (boardMap[starty][startx] == 0) {
+        return false;
+    } 
+    clicked = true
   }
   else {
     movePiece(startx, starty, Math.floor(e.offsetX/100), Math.floor(e.offsetY/100));
     reloadPos();
+    clicked = false
   }
-  console.log("clicked", clicked, e.offsetX/100, e.offsetY/100);
-  clicked = !clicked
+  
 });
+document.addEventListener("keydown", e => {
+    if (e.key === "Escape") {clicked = false; console.log("escaped")}
+})
--- js/piece.js
+++ js/piece.js
@@ -1,10 +1,18 @@
 import { boardMap } from "./data.js"
 
+
 export const movePiece = (x,y,newx,newy) => {
-  console.log("movePiece",x,y,newx,newy)
-  if (!boardMap[y][x]) {alert("no piece there"); return false}
-  if (newx > 7 || newy > 7 || newx < 0 || newy < 0) {alert("Out of bounds"); return false}
+  // console.log("movePiece",x,y,newx,newy)
+  // checks if there is a piece
+  if (!boardMap[y][x]) {return false}
+
+  // checks if the new and old positions contained the same team
+  let curIsUpper = boardMap[y][x] === boardMap[y][x].toString().toUpperCase()
+  let newIsUpper = boardMap[newy][newx] === boardMap[newy][newx].toString().toUpperCase()
+  if (curIsUpper === newIsUpper && !isFinite(boardMap[newy][newx])) {return false}
+  // if (newx > 7 || newy > 7 || newx < 0 || newy < 0) {alert("Out of bounds"); return false}
+  if (boardMap[newy][newx] == boardMap[y][x]) {return false}
   boardMap[newy][newx] = boardMap[y][x]
-  
   boardMap[y][x] = 0;
+  return true
 }
